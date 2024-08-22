6a1d1f7065d46ab46cbebf4f298b1604bbb2c892
QueenPM <danimark644@gmail.com>
2024-08-22 02:13:46 +0100

    wip: command refactor

--- code/GameSystems/Config/ICommandConfig.cs
+++ code/GameSystems/Config/ICommandConfig.cs
@@ -2,6 +2,18 @@ using GameSystems.Player;
 
 namespace GameSystems.Config
 {
+	public enum IArgumentType
+	{
+		Integer,
+		String,
+		Player
+	}
+	public interface IArgument
+	{
+		string Name { get; }
+		string Description { get; }
+		IArgumentType Type { get; }
+	}
 	/// <summary>
 	/// Interface for command configuration.
 	/// </summary>
@@ -14,6 +26,6 @@ namespace GameSystems.Config
 		bool ClientOnly { get; }
 
 		[Broadcast]
-		void CommandFunction( GameObject player, Scene scene, string[] args );
+		void CommandFunction( GameObject player, Scene scene, IArgument[] args );
 	}
 }
